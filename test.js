dbbe270e543aab9ce6b9e7ae275c69cc8f9f3667
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2020-11-09 08:22:01 -1000

    fix: handle types that can not be converted to string
    There was a bug where if a value passed in was not convertible to a
    string type, the library would throw
    
    TypeError: Cannot convert object to primitive value
    
    Any value that can not be converted to a string can not expose a
    secret AFAICT so thus these types should just pass through without
    issue. This patch implements that logic and adds tests with all object
    types to validate that the function works as expected.

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 'use strict'
 
-var test = require('tape')
 var isSecret = require('./')
+var test = require('tape')
+var util = require('util')
 
 test('isSecret.key true', function (t) {
   t.equal(isSecret.key('pass'), true)
@@ -30,4 +31,32 @@ test('isSecret.value credt card number', function (t) {
 test('isSecret.value false', function (t) {
   t.equal(isSecret.value('foobar'), false)
   t.end()
+})
+
+// Test all possible JS types minus string
+test('isSecret.value handling of types', function (t1) {
+  var typeTests = [
+    ['array of length 0', [], false],
+    ['boolean as false', false, false],
+    ['boolean as true', true, false],
+    ['function returning undefined', function () {}, false],
+    ['null', null, false],
+    ['number not 16 digits', 42, false],
+    ['number with 16 digits', 4242424242424242, true],
+    ['object with prototype', {}, false],
+    ['object without prototype', Object.create(null), false],
+    ['symbol foo', Symbol('foo'), false],
+    ['undefined', undefined, false]
+  ]
+  typeTests.forEach(function (testData) {
+    var data = testData[1]
+    var expt = testData[2]
+    var desc = util.format('value of %s returns %s', testData[0], expt)
+
+    t1.test(desc, function (t2) {
+      t2.equal(isSecret.value(data), expt)
+      t2.end()
+    })
+  })
+  t1.end()
 })
